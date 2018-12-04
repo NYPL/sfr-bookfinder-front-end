1651959120bbbd87843a219fe29de11c975d92fd
Gregory Kallenberg <gregorykallenberg@nypl.org>
2018-12-04 18:12:46 -0500

    call variable assignment for APP_ENV

--- webpack.config.js
+++ webpack.config.js
@@ -154,7 +154,7 @@ if (ENV === 'production') {
       new webpack.DefinePlugin({
         'process.env': {
           NODE_ENV: JSON.stringify('production'),
-          APP_ENV: JSON.stringify('production'),
+          APP_ENV: JSON.stringify(appEnv),
         },
       }),
     ],
