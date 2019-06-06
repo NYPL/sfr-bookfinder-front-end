aba0ca11c2a440cf04d1987675bec32144f26385
Pedro Carmona <nachocarmona@gmail.com>
2019-06-06 17:34:53 -0400

    Design for filter search

--- src/app/components/SearchResults/Filters.jsx
+++ src/app/components/SearchResults/Filters.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { isArray as _isArray } from 'underscore';
 import { initialSearchQuery, searchQueryPropTypes } from '../../stores/InitialState';
 import { getQueryString } from '../../search/query';
 
-const filtersLabels = { language: 'Language' };
+const filtersLabels = { language: 'Language', years: 'Puplication Year' };
 
 const Filters = ({
   data, searchQuery, userQuery, router,
@@ -12,7 +11,7 @@ const Filters = ({
   const filtersArray = [];
 
   // add search filters
-  if (searchQuery && searchQuery.filters && _isArray(searchQuery.filters)) {
+  if (searchQuery && searchQuery.filters && Array.isArray(searchQuery.filters)) {
     searchQuery.filters.forEach((filter) => {
       filtersArray.push({ field: filter.field, value: filter.value });
     });
@@ -83,6 +82,45 @@ const Filters = ({
     })
     .slice(0, 10);
 
+  const renderYearsFilter = () => (
+    <div className="grid-container padding-0">
+      <div className="grid-row">
+        <label
+          className="usa-label tablet:grid-col padding-right-4"
+          htmlFor="filters-years-start"
+        >
+          Start
+          <input
+            className="usa-input"
+            id="filters-years-start"
+            name="filters-years-start"
+            type="number"
+          />
+        </label>
+        <label
+          className="usa-label tablet:grid-col padding-right-4"
+          htmlFor="filters-years-end"
+        >
+          End
+          <input
+            className="usa-input"
+            id="filters-years-end"
+            name="filters-years-end"
+            type="number"
+          />
+        </label>
+      </div>
+      <div className="grid-row">
+        <button
+          className="usa-button usa-button--outline padding-x-4"
+          type="button"
+        >
+          Update
+        </button>
+      </div>
+    </div>
+  );
+
   if (data && data.facets && data.hits && data.hits.hits && data.hits.hits.length > 0) {
     return (
       <form className="filters usa-form">
@@ -92,29 +130,31 @@ const Filters = ({
             key={field}
             className="filters-box usa-fieldset"
           >
-            {data.facets[field] && <legend className="filters-box-header">{filtersLabels[field]}</legend>}
-            {prepareFilters(data.facets[field], field).map(facet => (
-              <div
-                className="usa-checkbox"
-                key={`facet-${field}-${facet.value}`}
-              >
-                <input
-                  className="usa-checkbox__input"
-                  id={`facet-${field}-${facet.value}`}
-                  type="checkbox"
-                  name={facet.value}
-                  onChange={e => onChangeCheckbox(e, field, facet.value)}
-                  checked={isFilterChecked(field, facet.value)}
-                />
-                <label
-                  className="usa-checkbox__label"
-                  htmlFor={`facet-${field}-${facet.value}`}
+            <legend className="filters-box-header">{filtersLabels[field]}</legend>
+            {field === 'years' && renderYearsFilter()}
+            {field !== 'years'
+              && prepareFilters(data.facets[field], field).map(facet => (
+                <div
+                  className="usa-checkbox"
+                  key={`facet-${field}-${facet.value}`}
                 >
-                  {facet.value}
-                  {facet.count > 0 && ` (${facet.count.toLocaleString()})`}
-                </label>
-              </div>
-            ))}
+                  <input
+                    className="usa-checkbox__input"
+                    id={`facet-${field}-${facet.value}`}
+                    type="checkbox"
+                    name={facet.value}
+                    onChange={e => onChangeCheckbox(e, field, facet.value)}
+                    checked={isFilterChecked(field, facet.value)}
+                  />
+                  <label
+                    className="usa-checkbox__label"
+                    htmlFor={`facet-${field}-${facet.value}`}
+                  >
+                    {facet.value}
+                    {facet.count > 0 && ` (${facet.count.toLocaleString()})`}
+                  </label>
+                </div>
+              ))}
           </fieldset>
         ))}
       </form>
