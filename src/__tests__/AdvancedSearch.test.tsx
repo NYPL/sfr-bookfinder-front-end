8b6008e0bc08b4ea3e74a4ce0fd183bb11a4edf6
Crystal Ngai <hokei@users.noreply.github.com>
2021-03-17 12:43:21 -0400

    Advanced Search Tests uncommented

--- src/__tests__/AdvancedSearch.test.tsx
+++ src/__tests__/AdvancedSearch.test.tsx
@@ -13,6 +13,8 @@ import { act, render, screen } from "@testing-library/react";
 import { FilterLanguagesCommonTests } from "./componentHelpers/FilterLanguages";
 import { FilterYearsTests } from "./componentHelpers/FilterYears";
 import { FilterFormatTests } from "./componentHelpers/FilterFormats";
+import userEvent from "@testing-library/user-event";
+import { errorMessagesText } from "../constants/labels";
 
 const defaultLanguages: FacetItem[] = [
   { value: "english", count: 25 },
@@ -64,103 +66,108 @@ describe("renders advanced search correctly", () => {
       expect(screen.getByRole("button", { name: "Clear" })).toBeInTheDocument();
     });
   });
+
+  test("Hides languages when no languages are passed", () => {
+    render(<AdvancedSearch searchQuery={initialSearchQuery} languages={[]} />);
+    expect(
+      screen.queryByRole("group", { name: "Languages" })
+    ).not.toBeInTheDocument();
+  });
+
+  test("Prepopulates form input based on search query parameters", () => {
+    render(
+      <AdvancedSearch
+        searchQuery={complicatedSearchQuery}
+        languages={defaultLanguages}
+      />
+    );
+
+    expect(screen.getByLabelText("english")).toBeChecked();
+    expect(screen.getByLabelText("From")).toHaveValue(1990);
+    expect(screen.getByLabelText("To")).toHaveValue(1999);
+    expect(screen.getByLabelText("Keyword")).toHaveValue("cat");
+  });
 });
 
-//   test("Hides languages when no languages are passed", () => {
-//     const wrapper = shallow(
-//       <AdvancedSearch searchQuery={initialSearchQuery} languages={[]} />
-//     );
-//     expect(wrapper.exists(LanguageAccordion)).toEqual(false);
-//   });
+describe("Advanced Search submit", () => {
+  test("Submits well formed query", () => {
+    render(
+      <MockNextRouterContextProvider>
+        <AdvancedSearch
+          searchQuery={complicatedSearchQuery}
+          languages={defaultLanguages}
+        />
+      </MockNextRouterContextProvider>
+    );
+    userEvent.click(screen.getByRole("button", { name: "Search" }));
 
-//   test("Prepopulates form input based on search query parameters", () => {
-//     const wrapper = mount(
-//       <AdvancedSearch
-//         searchQuery={complicatedSearchQuery}
-//         languages={defaultLanguages}
-//       />
-//     );
+    const expectedQuery = {
+      filters: `[{"field":"language","value":"english"},{"field":"years","value":{"start":1990,"end":1999}},{"field":"show_all","value":false}]`,
+      queries: `[{"field":"keyword","query":"cat"}]`,
+      per_page: "10",
+      sort: `[]`,
+    };
+    expect(mockPush).toHaveBeenCalledTimes(1);
+    expect(mockPush).toHaveBeenCalledWith({
+      pathname: "/search",
+      query: expectedQuery,
+    });
+  });
 
-//     expect(wrapper.find("#checkbox-english").props().checked).toEqual(true);
-//     expect(wrapper.find("input#date-filter-from").props().value).toEqual(1990);
-//     expect(wrapper.find("input#date-filter-to").props().value).toEqual(1999);
-//     expect(wrapper.find("input#keyword-input").props().value).toEqual("cat");
-//   });
-// });
+  test("shows error on empty query", () => {
+    render(
+      <MockNextRouterContextProvider>
+        <AdvancedSearch
+          searchQuery={initialSearchQuery}
+          languages={defaultLanguages}
+        />
+      </MockNextRouterContextProvider>
+    );
+    userEvent.click(screen.getByRole("button", { name: "Search" }));
+    expect(mockPush).toHaveBeenCalledTimes(0);
+    expect(screen.getByText(errorMessagesText.emptySearch)).toBeInTheDocument();
+  });
 
-// describe("Advanced Search submit", () => {
-//   test("Submits well formed query", () => {
-//     const wrapper = mount(
-//       <MockNextRouterContextProvider>
-//         <AdvancedSearch
-//           searchQuery={complicatedSearchQuery}
-//           languages={defaultLanguages}
-//         />
-//       </MockNextRouterContextProvider>
-//     );
-//     wrapper.find("form").simulate("submit");
+  test("show error on invalid year", () => {
+    const invalidYearSearch = Object.assign({}, initialSearchQuery, {
+      queries: [{ field: "keyword", query: "cat" }],
+      filterYears: { start: 1990, end: 1880 },
+    });
+    render(
+      <MockNextRouterContextProvider>
+        <AdvancedSearch
+          searchQuery={invalidYearSearch}
+          languages={defaultLanguages}
+        />
+      </MockNextRouterContextProvider>
+    );
+    userEvent.click(screen.getByRole("button", { name: "Search" }));
+    expect(mockPush).toHaveBeenCalledTimes(0);
+    expect(screen.getByText(errorMessagesText.invalidDate)).toBeInTheDocument();
+  });
+});
 
-//     const expectedQuery = {
-//       filters: `[{"field":"language","value":"english"},{"field":"years","value":{"start":1990,"end":1999}},{"field":"show_all","value":false}]`,
-//       queries: `[{"field":"keyword","query":"cat"}]`,
-//       per_page: "10",
-//       sort: `[]`,
-//     };
-//     expect(mockPush).toHaveBeenCalledTimes(1);
-//     expect(mockPush).toHaveBeenCalledWith({
-//       pathname: "/search",
-//       query: expectedQuery,
-//     });
-//   });
+describe("Advanced Search clear", () => {
+  test("Clears search", () => {
+    render(
+      <MockNextRouterContextProvider>
+        <AdvancedSearch
+          searchQuery={complicatedSearchQuery}
+          languages={defaultLanguages}
+        />
+      </MockNextRouterContextProvider>
+    );
 
-//   test("shows error on empty query", () => {
-//     const wrapper = mount(
-//       <MockNextRouterContextProvider>
-//         <AdvancedSearch
-//           searchQuery={initialSearchQuery}
-//           languages={defaultLanguages}
-//         />
-//       </MockNextRouterContextProvider>
-//     );
-//     wrapper.find("form").simulate("submit");
-//     expect(mockPush).toHaveBeenCalledTimes(0);
-//     expect(wrapper.exists(HelperErrorText)).toEqual(true);
-//     expect(wrapper.find(HelperErrorText)).toMatchSnapshot();
-//   });
+    expect(screen.getByLabelText("english")).toBeChecked();
+    expect(screen.getByLabelText("From")).toHaveValue(1990);
+    expect(screen.getByLabelText("To")).toHaveValue(1999);
+    expect(screen.getByLabelText("Keyword")).toHaveValue("cat");
 
-//   test("show error on invalid year", () => {
-//     const invalidYearSearch = Object.assign({}, initialSearchQuery, {
-//       queries: [{ field: "keyword", query: "cat" }],
-//       filterYears: { start: 1990, end: 1880 },
-//     });
-//     const wrapper = mount(
-//       <MockNextRouterContextProvider>
-//         <AdvancedSearch
-//           searchQuery={invalidYearSearch}
-//           languages={defaultLanguages}
-//         />
-//       </MockNextRouterContextProvider>
-//     );
-//     wrapper.find("form").simulate("submit");
-//     expect(mockPush).toHaveBeenCalledTimes(0);
-//     expect(wrapper.exists(HelperErrorText)).toEqual(true);
-//     expect(wrapper.find(HelperErrorText)).toMatchSnapshot();
-//   });
-// });
+    userEvent.click(screen.getByRole("button", { name: "Clear" }));
 
-// describe("Advanced Search clear", () => {
-//   test("Clears search", () => {
-//     const wrapper = mount(
-//       <MockNextRouterContextProvider>
-//         <AdvancedSearch
-//           searchQuery={complicatedSearchQuery}
-//           languages={defaultLanguages}
-//         />
-//       </MockNextRouterContextProvider>
-//     );
-//     expect(wrapper.find("#checkbox-english").props().checked).toEqual(true);
-
-//     wrapper.find(`#clear-button`).hostNodes().simulate("click");
-//     expect(wrapper.find("#checkbox-english").props().checked).toEqual(false);
-//   });
-// });
+    expect(screen.getByLabelText("english")).not.toBeChecked();
+    expect(screen.getByLabelText("From")).toHaveValue(null);
+    expect(screen.getByLabelText("To")).toHaveValue(null);
+    expect(screen.getByLabelText("Keyword")).toHaveValue("");
+  });
+});
