3ffaa2c17ccc71cf160275237b10681440954393
Crystal Ngai <hokei@users.noreply.github.com>
2021-06-02 10:07:01 -0400

    Edition page defaults to show everything

--- src/components/EditionDetail/Edition.tsx
+++ src/components/EditionDetail/Edition.tsx
@@ -102,7 +102,7 @@ const Edition: React.FC<{ editionResult: EditionResult }> = (props) => {
                       <>Show only items currently available online</>
                     ),
                   }}
-                  checked={router.query.showAll !== "true"}
+                  checked={router.query.showAll === "false"}
                   onChange={(e) => toggleShowAll(e)}
                 />
               </div>
