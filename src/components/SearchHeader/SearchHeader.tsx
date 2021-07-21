37aa2dc2bec0fcaf2af2b9e657ae76e959af030f
toxiapo <toxiapo@gmail.com>
2021-07-21 12:49:30 -0400

    remove searchQuery from AdvancedSearch component so it always perform a new search

--- src/components/SearchHeader/SearchHeader.tsx
+++ src/components/SearchHeader/SearchHeader.tsx
@@ -5,7 +5,6 @@ import * as DS from "@nypl/design-system-react-components";
 import SearchForm from "../SearchForm/SearchForm";
 import Link from "../Link/Link";
 import { SearchQuery } from "~/src/types/SearchQuery";
-import { toLocationQuery, toApiQuery } from "~/src/util/apiConversion";
 
 /**
  * Search Header Component
@@ -25,7 +24,6 @@ const SearchHeader: React.FC<{
           <Link
             to={{
               pathname: "/advanced-search",
-              query: searchQuery && toLocationQuery(toApiQuery(searchQuery)),
             }}
             modifiers={["dark-background"]}
             className="link"
--- src/pages/advanced-search/index.tsx
+++ src/pages/advanced-search/index.tsx
@@ -2,29 +2,24 @@ import React from "react";
 
 import AdvancedSearch from "~/src/components/AdvancedSearch/AdvancedSearch";
 import Layout from "~/src/components/Layout/Layout";
-import { toSearchQuery } from "~/src/util/apiConversion";
 import { languagesFetcher } from "../../lib/api/SearchApi";
 import { ApiLanguageResponse } from "../../types/LanguagesQuery";
-import { ApiSearchQuery } from "../../types/SearchQuery";
-export async function getServerSideProps(context: any) {
-  // Get Query from location
-  const searchQuery: ApiSearchQuery = context.query;
+export async function getServerSideProps() {
   // Fetch all languages
   const languageResponse: ApiLanguageResponse = await languagesFetcher();
   return {
     props: {
-      searchQuery: searchQuery.query ? toSearchQuery(searchQuery) : {},
       languages: languageResponse,
     },
   };
 }
 
 const AdvancedSearchPage: React.FC<any> = (props) => {
-  const { searchQuery, languages } = props;
+  const { languages } = props;
 
   return (
     <Layout>
-      <AdvancedSearch searchQuery={searchQuery} languages={languages} />
+      <AdvancedSearch languages={languages} />
     </Layout>
   );
 };
