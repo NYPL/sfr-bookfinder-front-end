822db9c6ec913ea644f9fbb5739a01993c58edb9
Jackie Quach <jackiequach14@gmail.com>
2022-06-16 16:24:42 -0400

    update snapshots to use generic classes

--- src/components/About/About.test.tsx
+++ src/components/About/About.test.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import AboutPage from "./About";
 import renderer from "react-test-renderer";
+import { createSerializer } from "@emotion/jest";
 jest.mock("next/router", () => require("next-router-mock"));
+
+expect.addSnapshotSerializer(createSerializer());
 
 it("renders about unchanged", async () => {
   const tree = await renderer.create(<AboutPage />).toJSON();
