5281a1932a6c77c635bdaf055d42f8c39a7dff29
Chris Mulholland <chrismulholland@nypl.org>
2023-01-05 08:42:27 -0500

    pub year filter tests edits

--- playwright/tests/pub_yearFilter.spec.ts
+++ playwright/tests/pub_yearFilter.spec.ts
@@ -2,30 +2,24 @@ import { Given, Then, When } from "@cucumber/cucumber";
 import { expect } from "@playwright/test";
 import { CustomWorld } from "../support/setup";
 
-Given(
-  "I want to filter by publication year",
-  async function (this: CustomWorld) {
-    return await this.page.goto(`${this.parameters.appUrl}`);
-  }
-);
-
-When(
-  "I search for New York to test publication year filter",
-  async function (this: CustomWorld) {
-    await this.page.locator('[aria-label="Item Search"]').fill("New York");
-    await this.page.locator("#searchbar-button-search-bar").click();
-  }
-);
-
-Then("I input publication years 1900-1900", async function (this: CustomWorld) {
-  await this.page
-    .locator('[aria-describedby="date-filter-from-helperText"]')
-    .fill("1900");
-  await this.page
-    .locator('[aria-describedby="date-filter-to-helperText"]')
-    .fill("1900");
-  await this.page.locator("#year-filter-button").click();
+Given("Given I am on the home page", async function (this: CustomWorld) {
+  return await this.page.goto(`${this.parameters.appUrl}`);
 });
+
+When("When I search for New York", async function (this: CustomWorld) {
+  await this.page.locator('[aria-label="Item Search"]').fill("New York");
+  await this.page.locator("#searchbar-button-search-bar").click();
+  // await this.page.getByRole("button", { name: /submit/i }).click();
+});
+
+Then(
+  "I filter by publication years 1900-1900",
+  async function (this: CustomWorld) {
+    await this.page.locator('[name="Date From"]').fill("1900");
+    await this.page.locator('[name="Date To"]').fill("1900");
+    await this.page.locator("#year-filter-button").click();
+  }
+);
 
 Then(
   "for at least 9 titles on the page, I should see a publication year of 1900",
