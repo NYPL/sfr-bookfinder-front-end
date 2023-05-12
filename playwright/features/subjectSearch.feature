e6f63c345779a057ff366bae4c6005998325c60e
clarissarichard <107963214+clarissarichard@users.noreply.github.com>
2023-05-12 15:12:17 -0400

    Refactored subjectSearch page visit actions and resolved details page timeout error

--- playwright/features/subjectSearch.feature
+++ playwright/features/subjectSearch.feature
@@ -1,7 +1,7 @@
 Feature: Subject Search
 
     Scenario: As a user I should be able to do an subject search and see relevent results
-        Given I want to do a subject search
+        Given I go to the "home" page
         When I change the dropdown to subject
         When I input a subject search term and submit
         Then Then for each title on the page, I visit the details page and expect to see the subject listed
--- playwright/tests/subjectSearch.spec.ts
+++ playwright/tests/subjectSearch.spec.ts
@@ -2,9 +2,6 @@ import { Given, Then, When } from "@cucumber/cucumber";
 import { expect } from "@playwright/test";
 import { CustomWorld } from "../support/setup";
 
-Given("I want to do a subject search", async function (this: CustomWorld) {
-  return await this.page.goto(`${this.parameters.appUrl}`);
-});
 When("I change the dropdown to subject", async function (this: CustomWorld) {
   const dropdown = await this.page.$("[aria-label='Select a search category']");
   await dropdown.selectOption({ value: "subject" });
@@ -20,18 +17,18 @@ When(
 
 Then(
   "Then for each title on the page, I visit the details page and expect to see the subject listed",
+  {timeout: 60000}, // for loop takes longer than the default 30000ms
   async function (this: CustomWorld) {
     const titles = this.page.locator("h2 a");
 
-    for (let i = 0; i < 10; ++i) {
-      await titles.nth(i);
+    for (let i = 0; i < 10; i++) {
       await titles.nth(i).click();
       // wait for All Editions heading to load so we know subjects are loaded
       await this.page
         .locator("//h3[contains(text(), 'All Editions')]")
         .waitFor();
-      const targetlink = this.page.locator("//a[starts-with(., 'Petroleum')]");
-      expect(await targetlink.isVisible()).toBe(true);
+      const targetlink = await this.page.getByRole("link", { name: "Petroleum", exact: true });
+      expect(await targetlink.isVisible());
       await this.page.goBack();
     }
   }
