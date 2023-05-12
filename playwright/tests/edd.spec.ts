f44a4feefacc2925b01ef02c03b28bcc2149478f
clarissarichard <107963214+clarissarichard@users.noreply.github.com>
2023-05-12 10:24:02 -0400

    Refactored edd page visit actions

--- playwright/tests/edd.spec.ts
+++ playwright/tests/edd.spec.ts
@@ -4,11 +4,11 @@ import { CustomWorld } from "../support/setup";
 import dotenv from "dotenv";
 dotenv.config({ path: ".env.local" });
 
-Given("I have done a keyword search", async function (this: CustomWorld) {
-  await this.page.goto(`${this.parameters.appUrl}`);
+When("I have done a keyword search", async function (this: CustomWorld) {
   await this.page.locator('[aria-label="Item Search"]').fill("Africa");
   await this.page.locator("#searchbar-button-search-bar").click();
 });
+
 When("I click the requestable box", async function (this: CustomWorld) {
   await this.page.locator("text=Requestable").click();
 });
@@ -19,14 +19,17 @@ Then(
     await this.page.locator("text=Log in for options >> nth=1").click();
   }
 );
+
 Then("I log in to the catalog", async function (this: CustomWorld) {
   await this.page.locator("#code").fill(process.env.CATALOG_USERNAME);
   await this.page.locator("#pin").fill(process.env.CATALOG_USER_PIN);
   await this.page.locator('input[type="submit"] >> text="Submit"').click();
 });
+
 Then("I click the first request button", async function (this: CustomWorld) {
-  await this.page.locator("text=Request >> nth=2").click();
+  await this.page.locator("text=Request").first().click();
 });
+
 Then("I expect to see delivery options", async function (this: CustomWorld) {
   await expect(this.page.locator("text=Have a small portion")).toBeVisible();
 });
