3217a8f2796bc44923557495d906f87e9f324ab0
clarissarichard <107963214+clarissarichard@users.noreply.github.com>
2023-05-24 15:20:44 -0400

    Refactored dropdown action in titleSearch

--- playwright/tests/titleSearch.spec.ts
+++ playwright/tests/titleSearch.spec.ts
@@ -2,11 +2,6 @@ import { Then, When } from "@cucumber/cucumber";
 import { expect } from "@playwright/test";
 import { CustomWorld } from "../support/setup";
 
-When("I change the dropdown to title", async function (this: CustomWorld) {
-  const dropdown = await this.page.$("[aria-label='Select a search category']");
-  await dropdown.selectOption({ value: "title" });
-});
-
 When("I input a title search term", async function (this: CustomWorld) {
   await this.page.locator("[aria-label='Item Search']").fill("IBM 1401");
 });
