1c382b1c57148c4352bf40cd3f06dea8674682bb
clarissarichard <107963214+clarissarichard@users.noreply.github.com>
2023-05-25 09:54:22 -0400

    Refactored dropdown action in authorSearch

--- playwright/features/authorSearch.feature
+++ playwright/features/authorSearch.feature
@@ -2,7 +2,7 @@ Feature: Author Search
 
     Scenario: As a user I should be able to do an author search and see relevant results
         Given I go to the "home" page
-        When I change the dropdown to author
+        When I select "author" from the category dropdown
         When I input an author search term
         When I click the "search button"
         Then I expect at least 5 title by my author
--- playwright/tests/authorSearch.spec.ts
+++ playwright/tests/authorSearch.spec.ts
@@ -2,11 +2,6 @@ import { Then, When } from "@cucumber/cucumber";
 import { expect } from "@playwright/test";
 import { CustomWorld } from "../support/setup";
 
-When("I change the dropdown to author", async function (this: CustomWorld) {
-  const dropdown = await this.page.$("[aria-label='Select a search category']");
-  await dropdown.selectOption({ value: "author" });
-});
-
 When("I input an author search term", async function (this: CustomWorld) {
   await this.page.locator("[aria-label='Item Search']").fill("Corelli, Marie");
 });
