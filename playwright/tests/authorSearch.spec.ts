a529ff56e2e1b21b9fc897486f8dcae1dedf9384
clarissarichard <107963214+clarissarichard@users.noreply.github.com>
2023-05-26 15:27:08 -0400

    Fixed lint errors

--- playwright/tests/authorSearch.spec.ts
+++ playwright/tests/authorSearch.spec.ts
@@ -9,9 +9,7 @@ When("I input an author search term", async function (this: CustomWorld) {
 Then(
   "I expect at least 5 titles by my author",
   async function (this: CustomWorld) {
-    await this.page
-      .locator("h1:text('Digital Research Books')")
-      .waitFor();
+    await this.page.locator("h1:text('Digital Research Books')").waitFor();
     const textIWant = this.page.locator("a", { hasText: " Corelli, Marie" });
     expect(await textIWant.count()).toBeGreaterThan(5);
   }
--- playwright/tests/titleSearch.spec.ts
+++ playwright/tests/titleSearch.spec.ts
@@ -9,9 +9,7 @@ When("I input a title search term", async function (this: CustomWorld) {
 Then(
   "I expect at least 5 titles with my title",
   async function (this: CustomWorld) {
-    await this.page
-      .locator("h1:text('Digital Research Books')")
-      .waitFor();
+    await this.page.locator("h1:text('Digital Research Books')").waitFor();
     const textIWant = this.page.locator('a:text("IBM 1401")');
     expect(await textIWant.count()).toBeGreaterThan(5);
   }
