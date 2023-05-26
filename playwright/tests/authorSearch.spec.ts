811fa76816ab065cb6e91fa597897bbad798cc50
clarissarichard <107963214+clarissarichard@users.noreply.github.com>
2023-05-26 15:12:23 -0400

    Optimized xpath locator to css in authorSearch

--- playwright/tests/authorSearch.spec.ts
+++ playwright/tests/authorSearch.spec.ts
@@ -10,7 +10,7 @@ Then(
   "I expect at least 5 titles by my author",
   async function (this: CustomWorld) {
     await this.page
-      .locator("//h1[contains(text(), 'Digital Research Books')]")
+      .locator("h1:text('Digital Research Books')")
       .waitFor();
     const textIWant = this.page.locator("a", { hasText: " Corelli, Marie" });
     expect(await textIWant.count()).toBeGreaterThan(5);
