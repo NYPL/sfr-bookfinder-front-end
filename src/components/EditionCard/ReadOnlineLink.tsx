92a5c941d5d74cf01949c3fd141629080a8479a9
Jackie Quach <jackiequach14@gmail.com>
2024-01-02 17:13:34 -0500

    add limited access ctas

--- src/components/EditionCard/ReadOnlineLink.tsx
+++ src/components/EditionCard/ReadOnlineLink.tsx
@@ -4,19 +4,27 @@ import Link from "~/src/components/Link/Link";
 import { ItemLink } from "~/src/types/DataModel";
 
 // "Read Online" button should only show up if the link was flagged as "reader" or "embed"
-const ReadOnlineLink: React.FC<{ readOnlineLink: ItemLink }> = ({
-  readOnlineLink,
-}) => {
+const ReadOnlineLink: React.FC<{
+  readOnlineLink: ItemLink;
+  isLoggedIn: boolean;
+}> = ({ readOnlineLink, isLoggedIn }) => {
+  let linkText = "Read Online";
+  let linkUrl: any = {
+    pathname: `/read/${readOnlineLink.link_id}`,
+  };
+
+  if (readOnlineLink.flags.nypl_login && !isLoggedIn) {
+    linkText = "Log in to read online";
+    linkUrl = `https://login.nypl.org/auth/login?redirect_uri=${encodeURIComponent(
+      window.location.href
+    )}`;
+  }
+
   return (
     readOnlineLink && (
       <Box>
-        <Link
-          to={{
-            pathname: `/read/${readOnlineLink.link_id}`,
-          }}
-          linkType="button"
-        >
-          Read Online
+        <Link to={linkUrl} linkType="button">
+          {linkText}
         </Link>
       </Box>
     )
