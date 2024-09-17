5c0bd25d5aefe2724ead5f561c78c433ea18d0d7
Jackie Quach <jackiequach14@gmail.com>
2024-09-17 13:28:32 -0400

    update fixture

--- src/__tests__/fixtures/CollectionFixture.ts
+++ src/__tests__/fixtures/CollectionFixture.ts
@@ -1,5 +1,5 @@
 import { CollectionResult } from "~/src/types/CollectionQuery";
-import { OpdsPublication } from "~/src/types/OpdsModel";
+import { Opds2Feed, OpdsPublication } from "~/src/types/OpdsModel";
 
 export const collections = [
   {
@@ -36,170 +36,168 @@ export const collections = [
   },
 ];
 
-export const oneCollectionListData: CollectionResult = {
-  collections: {
-    groups: [
-      {
-        links: [
-          {
-            href: "/collection/978ea0e0-8ecc-4de2-bfe8-032fea641d8e?page=1",
-            rel: ["self", "first", "previous", "next", "last"],
-            type: "application/opds+json",
-          },
-        ],
-        metadata: {
-          creator: "Mike Benowitz",
-          currentPage: 1,
-          description: "A history of the sport of baseball",
-          itemsPerPage: 5,
-          numberOfItems: 3,
-          title: "Baseball: A Collection by Mike Benowitz",
+export const oneCollectionListData: Opds2Feed = {
+  groups: [
+    {
+      links: [
+        {
+          href: "/collection/978ea0e0-8ecc-4de2-bfe8-032fea641d8e?page=1",
+          rel: ["self", "first", "previous", "next", "last"],
+          type: "application/opds+json",
         },
-        publications: [
-          {
-            editions: [],
-            images: [
-              {
-                href: "https://drb-files-qa.s3.amazonaws.com/covers/default/defaultCover.png",
-                type: "image/png",
-              },
-            ],
-            links: [
-              {
-                href: "www.nypl.org/research/collections/shared-collection-catalog/bib/b14514889",
-                rel: "http://opds-spec.org/acquisition/open-access",
-                type: "application/html+catalog",
-              },
-              {
-                href: "www.nypl.org/research/collections/shared-collection-catalog/hold/request/b14514889-i38116343",
-                rel: "http://opds-spec.org/acquisition/open-access",
-                type: "application/html+edd",
-              },
-              {
-                href: "https://drb-qa.nypl.org/edition/4267756",
-                rel: "alternate",
-                type: "text/html",
-              },
-            ],
-            metadata: {
-              alternate: [],
-              created: "Tue, 18 May 2021 10:00:41 GMT",
-              creator: "Wray, J. E. (J. Edward)",
-              description: '{"Master microform held by: NN."}',
-              language: "eng",
-              locationCreated: "New York (State)",
-              modified: "Tue, 18 May 2021 10:00:41 GMT",
-              published: 1900,
-              publisher: "American Sports Publishing,",
-              sortAs:
-                "how to organize a league, manage a team, captain a team, coach a team, score a game, arrange signals [microform] including how to lay out a league diamond, and technical terms of base ball,",
-              subtitle: null,
-              title:
-                "How to organize a league, manage a team, captain a team, coach a team, score a game, arrange signals [microform] including how to lay out a league diamond, and technical terms of base ball,",
-            },
-            type: "application/opds-publication+json",
-          },
-          {
-            editions: [],
-            images: [
-              {
-                href: "https://drb-files-qa.s3.amazonaws.com/covers/default/defaultCover.png",
-                type: "image/png",
-              },
-            ],
-            links: [
-              {
-                href: "catalog.hathitrust.org/api/volumes/oclc/67612618.html",
-                rel: "http://opds-spec.org/acquisition/open-access",
-                type: "text/html",
-              },
-              {
-                href: "https://drb-qa.nypl.org/edition/2422118",
-                rel: "alternate",
-                type: "text/html",
-              },
-            ],
-            metadata: {
-              ["@type"]: "http://schema.org/Book",
-              alternate: [],
-              created: "Thu, 22 Apr 2021 10:00:39 GMT",
-              creator: " Committee on Energy and Natural Resources.",
-              description: null,
-              language: "eng",
-              locationCreated: null,
-              modified: "Thu, 22 Apr 2021 10:00:39 GMT",
-              published: 2006,
-              publisher: "[U.S. G.P.O.],",
-              sortAs: "negro leagues baseball museum",
-              subtitle: null,
-              title: "Negro Leagues Baseball Museum",
-            },
-            type: "application/opds-publication+json",
-          },
-          {
-            editions: [],
-            images: [
-              {
-                href: "drb-files-qa.s3.amazonaws.com/covers/hathi/mdp.39015047482628.jpeg",
-                type: "image/jpeg",
-              },
-            ],
-            links: [
-              {
-                href: "babel.hathitrust.org/cgi/pt?id=mdp.39015047482628",
-                rel: "http://opds-spec.org/acquisition/open-access",
-                type: "text/html",
-              },
-              {
-                href: "catalog.hathitrust.org/api/volumes/oclc/275274.html",
-                rel: "http://opds-spec.org/acquisition/open-access",
-                type: "text/html",
-              },
-              {
-                href: "catalog.hathitrust.org/api/volumes/oclc/2192781.html",
-                rel: "http://opds-spec.org/acquisition/open-access",
-                type: "text/html",
-              },
-              {
-                href: "https://drb-qa.nypl.org/edition/894734",
-                rel: "alternate",
-                type: "text/html",
-              },
-            ],
-            metadata: {
-              "@type": "http://schema.org/Book",
-              alternate: [],
-              created: "Tue, 30 Mar 2021 19:29:20 GMT",
-              creator: "Spink, J. G. Taylor b. 1888.",
-              description: null,
-              language: "eng,und",
-              locationCreated: null,
-              modified: "Tue, 30 Mar 2021 19:29:20 GMT",
-              published: 1947,
-              publisher:
-                "Thomas Y. Crowell,, T. Y. Crowell Co., Crowell, New York",
-              sortAs: "judge landis and twenty-five years of baseball",
-              subtitle: null,
-              title: "Judge Landis and twenty-five years of baseball",
-            },
-            type: "application/opds-publication+json",
-          },
-        ],
+      ],
+      metadata: {
+        creator: "Mike Benowitz",
+        currentPage: 1,
+        description: "A history of the sport of baseball",
+        itemsPerPage: 5,
+        numberOfItems: 3,
+        title: "Baseball: A Collection by Mike Benowitz",
       },
-    ],
-    links: [
-      {
-        href: "/collection/list?page=1",
-        rel: ["self", "first", "previous", "next", "last"],
-        type: "application/opds+json",
-      },
-    ],
-    metadata: {
-      currentPage: 1,
-      itemsPerPage: 10,
-      numberOfItems: 1,
-      title: "Digital Research Books Collections",
+      publications: [
+        {
+          editions: [],
+          images: [
+            {
+              href: "https://drb-files-qa.s3.amazonaws.com/covers/default/defaultCover.png",
+              type: "image/png",
+            },
+          ],
+          links: [
+            {
+              href: "www.nypl.org/research/collections/shared-collection-catalog/bib/b14514889",
+              rel: "http://opds-spec.org/acquisition/open-access",
+              type: "application/html+catalog",
+            },
+            {
+              href: "www.nypl.org/research/collections/shared-collection-catalog/hold/request/b14514889-i38116343",
+              rel: "http://opds-spec.org/acquisition/open-access",
+              type: "application/html+edd",
+            },
+            {
+              href: "https://drb-qa.nypl.org/edition/4267756",
+              rel: "alternate",
+              type: "text/html",
+            },
+          ],
+          metadata: {
+            alternate: [],
+            created: "Tue, 18 May 2021 10:00:41 GMT",
+            creator: "Wray, J. E. (J. Edward)",
+            description: '{"Master microform held by: NN."}',
+            language: "eng",
+            locationCreated: "New York (State)",
+            modified: "Tue, 18 May 2021 10:00:41 GMT",
+            published: 1900,
+            publisher: "American Sports Publishing,",
+            sortAs:
+              "how to organize a league, manage a team, captain a team, coach a team, score a game, arrange signals [microform] including how to lay out a league diamond, and technical terms of base ball,",
+            subtitle: null,
+            title:
+              "How to organize a league, manage a team, captain a team, coach a team, score a game, arrange signals [microform] including how to lay out a league diamond, and technical terms of base ball,",
+          },
+          type: "application/opds-publication+json",
+        },
+        {
+          editions: [],
+          images: [
+            {
+              href: "https://drb-files-qa.s3.amazonaws.com/covers/default/defaultCover.png",
+              type: "image/png",
+            },
+          ],
+          links: [
+            {
+              href: "catalog.hathitrust.org/api/volumes/oclc/67612618.html",
+              rel: "http://opds-spec.org/acquisition/open-access",
+              type: "text/html",
+            },
+            {
+              href: "https://drb-qa.nypl.org/edition/2422118",
+              rel: "alternate",
+              type: "text/html",
+            },
+          ],
+          metadata: {
+            ["@type"]: "http://schema.org/Book",
+            alternate: [],
+            created: "Thu, 22 Apr 2021 10:00:39 GMT",
+            creator: " Committee on Energy and Natural Resources.",
+            description: null,
+            language: "eng",
+            locationCreated: null,
+            modified: "Thu, 22 Apr 2021 10:00:39 GMT",
+            published: 2006,
+            publisher: "[U.S. G.P.O.],",
+            sortAs: "negro leagues baseball museum",
+            subtitle: null,
+            title: "Negro Leagues Baseball Museum",
+          },
+          type: "application/opds-publication+json",
+        },
+        {
+          editions: [],
+          images: [
+            {
+              href: "drb-files-qa.s3.amazonaws.com/covers/hathi/mdp.39015047482628.jpeg",
+              type: "image/jpeg",
+            },
+          ],
+          links: [
+            {
+              href: "babel.hathitrust.org/cgi/pt?id=mdp.39015047482628",
+              rel: "http://opds-spec.org/acquisition/open-access",
+              type: "text/html",
+            },
+            {
+              href: "catalog.hathitrust.org/api/volumes/oclc/275274.html",
+              rel: "http://opds-spec.org/acquisition/open-access",
+              type: "text/html",
+            },
+            {
+              href: "catalog.hathitrust.org/api/volumes/oclc/2192781.html",
+              rel: "http://opds-spec.org/acquisition/open-access",
+              type: "text/html",
+            },
+            {
+              href: "https://drb-qa.nypl.org/edition/894734",
+              rel: "alternate",
+              type: "text/html",
+            },
+          ],
+          metadata: {
+            "@type": "http://schema.org/Book",
+            alternate: [],
+            created: "Tue, 30 Mar 2021 19:29:20 GMT",
+            creator: "Spink, J. G. Taylor b. 1888.",
+            description: null,
+            language: "eng,und",
+            locationCreated: null,
+            modified: "Tue, 30 Mar 2021 19:29:20 GMT",
+            published: 1947,
+            publisher:
+              "Thomas Y. Crowell,, T. Y. Crowell Co., Crowell, New York",
+            sortAs: "judge landis and twenty-five years of baseball",
+            subtitle: null,
+            title: "Judge Landis and twenty-five years of baseball",
+          },
+          type: "application/opds-publication+json",
+        },
+      ],
     },
+  ],
+  links: [
+    {
+      href: "/collection/list?page=1",
+      rel: ["self", "first", "previous", "next", "last"],
+      type: "application/opds+json",
+    },
+  ],
+  metadata: {
+    currentPage: 1,
+    itemsPerPage: 10,
+    numberOfItems: 1,
+    title: "Digital Research Books Collections",
   },
 };
 
