383932c1e83025acee60829680da2f8f5e69270a
Jackie Quach <jackiequach14@gmail.com>
2025-01-07 16:30:50 -0500

    add strategy for om script

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -65,6 +65,7 @@ class MyDocument extends Document<DocumentProps> {
           {/* OptinMonster */}
           <Script
             id="optinmonster"
+            strategy="afterInteractive"
             dangerouslySetInnerHTML={{
               __html: `(function(d,u,ac){var s=d.createElement('script');s.type='text/javascript';s.src='https://a.omappapi.com/app/js/api.min.js';s.async=true;s.dataset.user=u;s.dataset.account=ac;d.getElementsByTagName('head')[0].appendChild(s);})(document,12468,1044);`,
             }}
